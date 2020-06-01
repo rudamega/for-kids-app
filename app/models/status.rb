2f0bd4e774ad4b881703c42492a9cb2307ff39bd
ruben medina <ruben@megachopp.com.py>
2020-06-01 17:29:44 -0400

    Model user item orders created

---
+++ app/models/status.rb
@@ -0,0 +1,4 @@
+class Status < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+end
---
+++ db/migrate/20200601202408_add_columns_to_users.rb
@@ -0,0 +1,11 @@
+class AddColumnsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :address, :string
+    add_column :users, :district, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :country, :string
+    add_column :users, :cpf, :string
+    add_column :users, :credits, :integer
+  end
+end
