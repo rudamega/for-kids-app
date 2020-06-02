fc7ae7011e8500e76401625f4b24ddaa474c92fc
ruben medina <ruben@megachopp.com.py>
2020-06-02 07:34:25 -0400

    inseri o select 2 no country pra ele ficar mais bonithinho

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+import { initSelect2 } from '../components/init_select2';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -31,4 +31,5 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initSelect2();
 });
