7c93d7c6189a1330231ce742ad52dc5aeb9f4ac4
ruben medina <ruben@megachopp.com.py>
2020-06-02 16:36:06 -0400

    avatar user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :district, :city, :state, :country])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :district, :city, :state, :country, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
