8fc14938ce741a1a11fea161ed00f64b32bb70ab
ruben medina <ruben@megachopp.com.py>
2020-06-02 15:06:23 -0400

    coloquei o sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'items#index'
+  root to: 'pages#home'
   resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
