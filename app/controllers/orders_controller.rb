e9760e6a97c38b34f52c96c70f225ad8df1ff964
Paulo Sacilotti <paulo.sacilotti@gmail.com>
2020-06-03 15:32:20 -0300

    orders controler and view

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,26 @@
 class OrdersController < ApplicationController
   def index
+    @orders = Order.all
   end
 
   def new
-  
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.save
+    redirect_to
   end
 
   def show
+    @order=Order.find(params[:id])
   end
+
+
+private
+def order_params
+  params.require(:order).permit(:status, :user_id, :item_id)
+end
   
 end
