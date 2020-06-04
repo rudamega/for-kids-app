37e3b870eb46e1be73d98edf7671c3151f2ce4f9
ruben medina <ruben@megachopp.com.py>
2020-06-04 13:10:26 -0400

    nao me lembro

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :items do
+  resources :pages, only: :index
+    resources :items do
     resources :orders, only: [:create, :new]
   end
   resources :orders, only: [:index]
