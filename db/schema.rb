fb509dc7b220c23abb169b7464f9194aea35db58
Paulo Sacilotti <paulo.sacilotti@gmail.com>
2020-06-04 11:29:00 -0300

    adding sold status to Item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_181220) do
+ActiveRecord::Schema.define(version: 2020_06_04_142435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_06_02_181220) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "sold", default: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
